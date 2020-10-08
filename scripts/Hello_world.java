d1b4094bf657023956a84f3b78195cbfaf5bf3d8
Alan Paul <57307037+Alanzz001@users.noreply.github.com>
2020-10-08 10:47:37 +0530

    Create Hello_world.java
    First java program

---
+++ scripts/Hello_world.java
@@ -0,0 +1,12 @@
+ //LANGUAGE : JAVA
+ // AUTHOR   : ALAN PAUL
+ // GITHUB   : https://www.github.com/Alanzz001
+ 
+ 
+  class Hello{
+   public static void main (String args[])
+   {
+     System.out.println("Hello World!!!");
+    }
+   }
+   
